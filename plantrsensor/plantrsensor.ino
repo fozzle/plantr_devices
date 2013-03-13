dcfbc0576ec05656c276ad3ec67f47d45b40eb32
Kyle Petrovich <kylepetrovich@gmail.com>
2013-03-13 19:36:00 -0400

    Added arduino files

---
+++ plantrsensor/plantrsensor.ino
@@ -0,0 +1,25 @@
+int lightPin = 0;
+int sensorValue = 0;
+int moisturePin = 1;
+int powerPin = 2;
+int moistureValue;
+String id = String("yN8a1-HDe0g");
+
+void setup() {
+  pinMode(powerPin, OUTPUT);
+  digitalWrite(powerPin, HIGH);
+  Serial.begin(9600);
+}
+
+void loop() {
+  // Read and store light value in string
+  sensorValue = analogRead(lightPin);
+  String light = String(int(sensorValue));
+  
+  // Read and store moisture value in string
+  moistureValue = analogRead(moisturePin);
+  String moisture = String(int(moistureValue));
+  
+  Serial.println(String("i"+id+"l"+light+"m"+moisture));
+  delay(2000);
+}
